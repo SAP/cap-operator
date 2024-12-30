0241e0d9cf28e9b3d6c15b069a7bcf3a601aabba
Pavan <25031267+Pavan-SAP@users.noreply.github.com>
2024-12-30 23:01:31 +0000

    [Misc]  Year updated
    update year in relevant files

--- pkg/apis/sme.sap.com/v1alpha1/doc.go
+++ pkg/apis/sme.sap.com/v1alpha1/doc.go
@@ -2,7 +2,7 @@
 // +groupName=sme.sap.com
 
 /*
-SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and cap-operator contributors
+SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and cap-operator contributors
 SPDX-License-Identifier: Apache-2.0
 */
 
